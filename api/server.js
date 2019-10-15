0d2469a46297aad704299b68b337fc15fb2ff284
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-10-15 15:44:47 -0600

    Routers added

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const server = express()
+const cors = require('cors');
+const helmet = require('helmet');
+const server = express();
 
+const teamRouter = require('../routers/teams/team-router');
+
+server.use(helmet());
+server.use(cors());
 server.use(express.json())
+
+server.use('/api/teams', teamRouter);
 
 server.get('/', (req, res) => {
     res.send('api is up')
 });
+
+server.all('*', (req,res)=>{
+    res.status(404).send({ message: "How did you get here?" })
+  })
 
 module.exports = server
