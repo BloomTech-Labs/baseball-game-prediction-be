a9a2a4925060ed34e1cbc0fb6b0ae21115e3e03b
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-10-15 16:49:06 -0600

    Game Router added, need to test all endpoints

--- api/server.js
+++ api/server.js
@@ -4,12 +4,14 @@ const helmet = require('helmet');
 const server = express();
 
 const teamRouter = require('../routers/teams/team-router');
+const gameRouter = require('../routers/games/game-router');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json())
 
 server.use('/api/teams', teamRouter);
+server.use('/api/games', gameRouter);
 
 server.get('/', (req, res) => {
     res.send('api is up')
---
+++ routers/games/game-router.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const db = require('./game-model');
+
+// Get all games
+
+router.get('/', (req, res) => {
+    db.getGames()
+    .then(games => {
+        res.status(200).json(games)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting games" })
+    });
+})
+
+// Get individual game
+
+router.get('/:game_id', (req, res) => {
+    const { game_id } = req.params;
+    db.getGameById(game_id)
+    .then(game => {
+        res.status(200).json(game)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting game" })
+    });
+})
+
+// Get games from a certain team
+
+router.get('/:team_id', (req, res) => {
+    const { team_id } = req.params;
+    console.log(team_id)
+    db.getGamesByTeamId(team_id)
+    .then(games => {
+        res.status(200).json(games)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting games from that team" })
+    });
+})
+
+// Get games from a certain date
+
+router.get('/date', (req, res) => {
+    const date = req.body
+    db.getGamesByDate(date)
+    .then(games => {
+        res.status(200).json(games)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting games from that date" })
+    });
+})
+
+// Get completed or incomplete games
+
+router.get('/completed', (req, res) => {
+    const { completed } = req.body
+    db.getGamesByCompleted(completed)
+    .then(games => {
+        res.status(200).json(games)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error getting games" })
+    });
+})
+
+module.exports = router;
