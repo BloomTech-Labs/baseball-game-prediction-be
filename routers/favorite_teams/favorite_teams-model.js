93e6ef79ed3d9163adcadac69eb535022dd9942e
Michael Mangus <mjmangus16@yahoo.com>
2019-10-16 09:10:27 -0400

    added route for getting favorite teams by profile id

--- routers/favorite_teams/favorite_teams-model.js
+++ routers/favorite_teams/favorite_teams-model.js
@@ -1,9 +1,14 @@
 const db = require("../../data/dbConfig");
 
 module.exports = {
-  getFavoriteTeams
+  getFavoriteTeams,
+  getFavoriteTeamsByProfileId
 };
 
 function getFavoriteTeams() {
   return db("favorite_teams").select("*");
+}
+
+function getFavoriteTeamsByProfileId(profile_id) {
+  return db("favorite_teams").where("profile_id", profile_id);
 }
--- routers/favorite_teams/favorite_teams-router.js
+++ routers/favorite_teams/favorite_teams-router.js
@@ -20,4 +20,24 @@ router.get("/", (req, res) => {
     });
 });
 
+// Get favorite teams by profile id
+
+router.get("/:profile_id", (req, res) => {
+  const { profile_id } = req.params;
+  db.getFavoriteTeamsByProfileId(profile_id)
+    .then(favorites => {
+      if (favorites) {
+        res.status(200).json(favorites);
+      } else {
+        res
+          .status(500)
+          .json({ error: "That profile does not have any favorite teams" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Server error getting favorite teams" });
+    });
+});
+
 module.exports = router;
