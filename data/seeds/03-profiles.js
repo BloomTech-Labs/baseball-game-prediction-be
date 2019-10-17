f99c0622e6173dd9f39ef251baa77374b30bf87f
Michael Mangus <mjmangus16@yahoo.com>
2019-10-17 09:00:42 -0400

    made some adjustments to seed files

--- data/seeds/03-profiles.js
+++ data/seeds/03-profiles.js
@@ -1,13 +1,8 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("profiles")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("profiles").insert([
-        { profile_id: 1, firebase_id: "123456" },
-        { profile_id: 2, firebase_id: "234567" },
-        { profile_id: 3, firebase_id: "345678" }
-      ]);
-    });
+  // Inserts seed entries
+  return knex("profiles").insert([
+    { profile_id: 1, firebase_id: "123456" },
+    { profile_id: 2, firebase_id: "234567" },
+    { profile_id: 3, firebase_id: "345678" }
+  ]);
 };
