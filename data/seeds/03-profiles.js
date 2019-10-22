dd90b67ad1fe6d0499347045385629cda28a9514
Adam Hoffman <wintros@yahoo.com>
2019-10-22 14:33:55 -0600

    Changed email to username

--- data/seeds/03-profiles.js
+++ data/seeds/03-profiles.js
@@ -6,9 +6,9 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("profiles").insert([
-        { profile_id: 1, /*firebase_id: "123456"*/ email: 'sillysally@gmail.com', password: 'password' },
-        { profile_id: 2, /*firebase_id: "234567"*/ email: 'johnnyrock@yahoo.com', password: 'password' },
-        { profile_id: 3, /*firebase_id: "345678"*/ email: 'katyoshae@comcast.net', password: 'password' }
+        { profile_id: 1, /*firebase_id: "123456"*/ username: 'sillysally', password: 'password' },
+        { profile_id: 2, /*firebase_id: "234567"*/ username: 'johnnyrock', password: 'password' },
+        { profile_id: 3, /*firebase_id: "345678"*/ username: 'katyoshae', password: 'password' }
       ]);
     });
 };
